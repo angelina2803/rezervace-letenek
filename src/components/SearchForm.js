572b80fecf626719ab7592809e1f2469127e9670
Angelina Yumatova <>
2023-06-09 14:31:14 +0200

    .

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,16 +4,15 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Button from '@mui/material/Button';
 import MyInput from "./UI/input/MyInput";
-import MyButton from "./UI/button/MyButton" 
-
 
 const SearchForm = ({ create }) => {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
   const [value, setValue] = React.useState(dayjs("2022-04-17"));
   const [duration, setDuration] = useState("");
-  
+
   const addNewPost = (e) => {
     e.preventDefault();
     const newPost = {
@@ -22,7 +21,7 @@ const SearchForm = ({ create }) => {
       to,
       value,
       duration,
-    //   selectedDate: formatSelectedDate(selectedDate.toISOString()),
+      //   selectedDate: formatSelectedDate(selectedDate.toISOString()),
     };
     create(newPost);
     setFrom("");
@@ -33,7 +32,8 @@ const SearchForm = ({ create }) => {
 
   return (
     <form className="formAdd">
-      <MyInput
+      <MyInput 
+        id="myInput"
         value={from}
         onChange={(e) => setFrom(e.target.value)}
         type="text"
@@ -61,12 +61,27 @@ const SearchForm = ({ create }) => {
         type="text"
         placeholder="Délka letů"
       ></MyInput>
-      <MyButton className="myBtn" onClick={addNewPost} >Hledat <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-  <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/>
-  <path fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"/>
-</svg></MyButton> 
-      
+      <Button variant="outlined" className="myBtn" onClick={addNewPost}>
+        Vyhledat{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-zoom-in"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
+          />
+          <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z" />
+          <path
+            fill-rule="evenodd"
+            d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"
+          />
+        </svg>
+      </Button>
     </form>
   );
 };
