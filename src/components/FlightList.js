25cca421d8b841d00d1b7c9b6e9007d89802e397
Angelina Yumatova <>
2023-06-12 14:41:07 +0200

    change

--- src/components/FlightList.js
+++ src/components/FlightList.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import Button from "@mui/material/Button";
-import RezervationForm from "./RezervationForm";
 
 const FlightList = ({ flights = [] }) => {
     const columns = [
@@ -12,7 +10,7 @@ const FlightList = ({ flights = [] }) => {
         { field: "duration", headerName: "Duration", width: 130 },
         { field: "price", headerName: "Price", width: 130 },
       ];
-      
+
   return (
     <div>
     <div style={{ height: 400, width: "100%", marginTop: '30px', marginBottom:'30px'}}>
