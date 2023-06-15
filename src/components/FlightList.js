669688fc5285f167137a5a147ea1015b18686e5b
Angelina Yumatova <>
2023-06-15 09:43:12 +0200

    context React and select

--- src/components/FlightList.js
+++ src/components/FlightList.js
@@ -1,32 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { GlobalContext } from "./context/GlobalContext";
 
-const FlightList = ({ flights = []}) => {
-    const columns = [
-        { field: "id", headerName: "ID", width: 70 },
-        { field: "from", headerName: "From", width: 80 },
-        { field: "to", headerName: "To", width: 130 },
-        { field: "departure", headerName: "Departure", width: 180 },
-        { field: "arrival", headerName: " Arrival", width: 180 },
-        { field: "duration", headerName: "Duration", width: 130 },
-        { field: "price", headerName: "Price", width: 130 },
-      ];
+const FlightList = () => {
+  const { filteredFlights, setSelectedFlight } = useContext(GlobalContext);
+  const [selection, setSelection] = useState([]);
 
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "from", headerName: "From", width: 80 },
+    { field: "to", headerName: "To", width: 130 },
+    { field: "departure", headerName: "Departure", width: 180 },
+    { field: "arrival", headerName: " Arrival", width: 180 },
+    { field: "duration", headerName: "Duration", width: 130 },
+    { field: "price", headerName: "Price", width: 130 },
+  ];
+  const handleRowSelect = (newSelectionId) => {
+    setSelectedFlight(
+      filteredFlights.find((flight) => flight.id === newSelectionId[0])
+    );
+    setSelection(newSelectionId);
+  };
   return (
     <div>
-    <div style={{ height: 400, width: "100%", marginTop: '30px', marginBottom:'30px'}}>
-      <DataGrid
-        className="flightList"
-        rows={flights}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
+      <div
+        style={{
+          height: 400,
+          width: "100%",
+          marginTop: "30px",
+          marginBottom: "30px",
         }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
+      >
+        <DataGrid
+          className="flightList"
+          rows={filteredFlights}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          rowSelectionModel={selection}
+          onRowSelectionModelChange={handleRowSelect}
+        />
       </div>
     </div>
   );
